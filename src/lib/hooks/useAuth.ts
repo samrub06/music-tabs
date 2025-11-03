b566c94a8b6eca3f7e2ed54cd71cb21ecd9987cc
samrub06 <samrub06@gmail.com>
2025-11-03 15:13:06 +0200

    fixAuth

--- src/lib/hooks/useAuth.ts
+++ src/lib/hooks/useAuth.ts
@@ -69,13 +69,13 @@ export function useAuth() {
     try {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
-        options: {
-          redirectTo: `${window.location.origin}/api/auth/callback`,
+       /*  options: {
+          redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`,
           queryParams: {
             access_type: 'offline',
             prompt: 'consent',
           },
-        },
+        }, */
       });
       if (error) throw error;
     } catch (error) {
