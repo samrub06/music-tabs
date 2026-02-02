04aa3d1b34dcbaf2fabee6fc838afa0e86a96da6
samrub06 <samrub06@gmail.com>
2026-02-02 05:02:13 +0200

    ok

--- src/app/song/layout.tsx
+++ src/app/song/layout.tsx
@@ -1,10 +1,8 @@
-import Header from '@/components/Header'
 import { ReactNode } from 'react'
 
 export default function SongLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <Header />
       <div className="flex-1 flex flex-col overflow-hidden">
         {children}
       </div>
